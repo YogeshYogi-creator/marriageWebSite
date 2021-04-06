ec4e81c769d19c75293c4da6102108b26ae40802
Yogesh BL <65061354+YogeshYogi-creator@users.noreply.github.com>
2021-04-06 09:16:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Welcome from './Welcome'
 import Contact from './Contact'
 import {Link, Route} from 'react-router-dom'
-import {Grid, Paper, Button, Typography} from '@material-ui/core'
+import {Grid, Paper, Button, Typography, Container} from '@material-ui/core'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 const App = (props) => {
-
+  useEffect(()=>{
+    <Welcome/>
+  })
   return (
+    <Container>
     <Grid>
       <Paper>
         <Grid container>
         <Grid style = {{textAlign: "center"}}>
-            <Button component = {Link} to = "./"><img width= "80%" height= "100%" alt = "title_img" src="https://static1.squarespace.com/static/5a5683a5dc2b4a49e4390ac4/t/5e99cc31e967a27db493a3f2/1617272666008/?format=1500w"/></Button>
+            <Button component = {Link} to = "./welcome"><img width= "80%" height= "100%" alt = "title_img" src="https://static1.squarespace.com/static/5a5683a5dc2b4a49e4390ac4/t/5e99cc31e967a27db493a3f2/1617272666008/?format=1500w"/></Button>
         </Grid>
         </Grid>
         <Grid container>
@@ -25,13 +29,14 @@ const App = (props) => {
           </Grid>
         </Grid>
         <hr/>
-        <Button style = {{color: "#8d6235"}} component = {Link} to = "/">Welcome</Button>
+        <Button style = {{color: "#8d6235"}} component = {Link} to = "/welcome">Welcome</Button>
         <Button style = {{color: "#8d6235"}} component = {Link} to = "/contact">Contact</Button>
         <hr/>
-        <Route path = "/" component = {Welcome} exact = {true}/>
+        <Route path = "/welcome" component = {Welcome} exact = {true}/>
         <Route path = "/contact" component = {Contact}/>
       </Paper>
       </Grid>
+      </Container>
   )
 }
 export default App
